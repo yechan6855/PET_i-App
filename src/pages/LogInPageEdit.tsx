6d4f1f8a45cc5780ba0337b97caf2ed637f172d2
ji0ne <zi0neeeeeeeee@gmail.com>
2024-07-18 20:08:06 +0900

    LoginPage

--- src/pages/LogInPageEdit.tsx
+++ src/pages/LogInPageEdit.tsx
@@ -1,42 +1,111 @@
 import React from 'react';
-import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, ImageBackground } from 'react-native';
 
 import textLogoWhite from '../../assets/images/peti-text-icon-w.png';
 
-
+const backgroundImage = require('../../assets/images/background.jpg');
 
 const styles = StyleSheet.create({
+
+    BackgroundImage : {
+        flex: 1,
+        justifyContent: 'center'
+    },
+
     Container: {
-        height : '100%',
-        margin:40,
+        //최상위 부모_상단 / 하단 공백, 수직정렬
+        width : '100%',
+        height: '80%',
+        marginTop:40,
+        marginBottom:20,
         flexDirection: 'column',
-        backgroundColor : '#d2ea2d'
+        //backgroundColor : '#d2ea2d'
     },
     TopTextLogo:{
         height: 50,
         width: '100%',
         resizeMode: 'contain',
-        backgroundColor : '#2de8ea'
+        //backgroundColor : '#2de8ea'
     },
     TextInputContainer: {
-        flex: 0.3,
-        backgroundColor: '#2de8ea',
-        marginTop: '20%',
+        height: '30%',
+        //backgroundColor: '#2de8ea', //아쿠아색
+        marginTop: '10%',
+        padding : '4%',
+        alignItems: 'center',
         flexDirection: 'column',
     },
     TextInputItem: {
         width:'100%',
-        height:'30%',
-        backgroundColor: 'black',
+        height:'40%',
+        marginBottom: '2%',
+        //backgroundColor: 'black',
+        alignItems: 'center',
         flexDirection: 'row',
+        //padding : '1%'
     },
-    TextInputLabel:{
+    TextInputLabel:{ 
+        width : 60,
         fontSize:20,
         fontWeight:'bold',
-        marginEnd:'80%',
+        //backgroundColor:'white',
+        color : 'white',
+        textAlign : 'center',
+        //justifyContent : 'center'
+    },
+    input:{
+        width : '80%',
+        textAlign : 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: 'white',
+        //backgroundColor : '#d2ea2d'
+    },
+
+
+
+
+    LoginBtnContainer:{
+        height:'25%',
+        marginTop:'10%',
+        flexDirection: 'column',
+        padding : '6%',
+        //backgroundColor: '#2de8ea',
+    },
+
+    LoginBtn:{
+        height:'50%',
+        borderRadius:20,
+        padding : 6,
+        backgroundColor:'orange',
+        margin : 5
+    },
+    SigninBtn:{
+        height:'50%',
+        borderRadius:20,
         backgroundColor:'white',
-        paddingTop:'3%',
-        paddingEnd:'3%'
+        padding : 6,
+        margin : 5
+    },
+    LoginBtnText : {
+        fontSize:15,
+        fontWeight: 'bold',
+        color : 'white',
+        textAlign: 'center',
+    },
+    SigninBtnText : {
+        fontSize:15,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+
+    TextFindingIDPw : {
+        textAlign : 'center',
+        color : 'white'
+    },
+    FindingIDPw : {
+        position : 'absolute',
+        bottom: 0,
+        width: '100%'
     }
 
 
@@ -44,6 +113,7 @@ const styles = StyleSheet.create({
 
 function LoginPageEdit() {
     return (
+        <ImageBackground source={backgroundImage} style={styles.BackgroundImage} resizeMode="cover">
         <View style={styles.Container}>
             <View>
                 <Image
@@ -54,20 +124,42 @@ function LoginPageEdit() {
             <View style={styles.TextInputContainer}>
                 <View style ={styles.TextInputItem}>
                     <Text style ={styles.TextInputLabel}>ID</Text>
-
-                </View>
+                        <TextInput
+                        style = {styles.input}
+                        placeholder="ID를 입력해주세요"
+                        placeholderTextColor='white'
+                        />
+                    </View>
                 <View style ={styles.TextInputItem}>
                     <Text style ={styles.TextInputLabel}>PW</Text>
-
+                    <TextInput
+                        style = {styles.input}
+                        placeholder="비밀번호를 입력해주세요"
+                        placeholderTextColor='white'
+                        />
                 </View>
                 
             </View>
-            
 
+            <View style = {styles.LoginBtnContainer}>
+                <TouchableOpacity style = {styles.LoginBtn}>
+                    <Text style = {styles.LoginBtnText}>로그인</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style = {styles.SigninBtn}>
+                    <Text style = {styles.SigninBtnText}>회원가입</Text>
+                </TouchableOpacity>
+            </View>
+            
+            <View style = {styles.FindingIDPw}>
+            <TouchableOpacity>
+                <Text style = {styles.TextFindingIDPw}>ID / 비밀번호 찾기</Text>
+            </TouchableOpacity>
+            </View>
 
 
 
         </View>
+        </ImageBackground>
 
     );
 }
