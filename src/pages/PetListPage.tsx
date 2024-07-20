4bc11fa5ec9cfb44e275448302da107560d9a22c
ptk57581 <ptk725739@gmail.com>
2024-07-20 22:46:30 +0900

    pet list page publishing

--- src/pages/PetListPage.tsx
+++ src/pages/PetListPage.tsx
@@ -12,19 +12,26 @@ const styles = StyleSheet.create({
         backgroundColor : 'black'
     },
     petListEditContainer :{
-        width : '100%',
         height : '10%',
         flexDirection : 'row',
         backgroundColor : 'white',
     },
+    tipText : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center',
+        // backgroundColor : 'red',
+    },
     userNameSection : {
         width : '80%',
-        backgroundColor : '#2de8ea',
+        // backgroundColor : '#2de8ea',
+        fontSize : 22
     },
     editIconSection : {
         width : '20%',
         height : '100%',
-        alignItems : 'center'
+        alignItems : 'center',
+        justifyContent : 'center'
     },
     editIconImage :{
         width : '70%',
@@ -32,33 +39,33 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
     },
     petListSection :{
-       margin : '5%',
-        width : '90%',
-        height : '100%',
-        backgroundColor : 'white'
+        backgroundColor : 'white',
+        flex : 1,
+        flexDirection : 'row',
+        flexWrap : 'wrap',
+        justifyContent : 'space-between',
+        padding : 16,
     },
     petListItemContainer :{
         width : '45%',
-        height : '35%',
-        margin : '2%',
-        flexDirection : 'column',
-        backgroundColor : '#ea5a2d',
-        alignItems : 'center'
+        height : 200,
+        backgroundColor : '#FFF0E5',
+        alignItems : 'center',
+        justifyContent : 'center',
+        marginBottom : 16,
+        borderRadius : 16
     },
     petListItemImg :{
-        height : '70%',
-        width : '80%',
-        resizeMode: 'contain',
-
+        width : 100,
+        height : 100,
+        marginBottom : 16,
+        borderRadius : 50
     },
     petListItemTextContainer :{
-        height : '25%',
-        width : '100%',
-        backgroundColor : '#2de8ea',
-        justifyContent : 'center'
     },
     petListItemTest :{
         textAlign : 'center',
+        fontWeight : 600
     }
 
 });
@@ -68,36 +75,67 @@ function PetList()
     return(
         <View style = {styles.container}>
             <View style = {styles.petListEditContainer}>
-                <Text style = {styles.userNameSection}>정예림 님의 아이들</Text>
+                <View style={styles.tipText}>
+                    <Text style = {styles.userNameSection}>정예림 님의 아이들</Text>
+                </View>
                 <TouchableOpacity style = {styles.editIconSection}>
                     <Image
-                    source={editIcon}
-                    style = {styles.editIconImage}
+                        source={editIcon}
+                        style = {styles.editIconImage}
                     />
                 </TouchableOpacity>
             </View>
-
             <View style = {styles.petListSection}>
-
+                <Item
+                    name='살구'
+                    birth='2014.09.04'
+                    image='이미지임'
+                    petId='1'
+                />
                 <View style = {styles.petListItemContainer}>
                     <Image
-                    source={petProfileImg}
-                    style = {styles.petListItemImg}
+                        source={petProfileImg}
+                        style = {styles.petListItemImg}
                     />
                     <View style = {styles.petListItemTextContainer}>
                         <Text style = {styles.petListItemTest}>살구</Text>
                         <Text style = {styles.petListItemTest}>2014.09.03</Text>
                     </View>
-
-                    
                 </View>
-
-
+                <View style = {styles.petListItemContainer}>
+                    <Image
+                        source={petProfileImg}
+                        style = {styles.petListItemImg}
+                    />
+                    <View style = {styles.petListItemTextContainer}>
+                        <Text style = {styles.petListItemTest}>살구</Text>
+                        <Text style = {styles.petListItemTest}>2014.09.03</Text>
+                    </View>
+                </View>
             </View>
-        
         </View>
-
     );
+}
+
+interface PetItemProp {
+    name : string;
+    birth : string;
+    petId : string;
+    image : string;
+}
+function Item(prop : PetItemProp) {
+    return (
+        <View style = {styles.petListItemContainer}>
+            <Image
+                source={petProfileImg}
+                style = {styles.petListItemImg}
+            />
+            <View style = {styles.petListItemTextContainer}>
+                <Text style = {styles.petListItemTest}>{prop.name}</Text>
+                <Text style = {styles.petListItemTest}>{prop.birth}</Text>
+            </View>
+        </View>
+    )
 }
 
 export default PetList;
